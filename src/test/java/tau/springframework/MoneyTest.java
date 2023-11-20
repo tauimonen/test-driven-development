9fbada3e3d5b860955740f745f534b853bd5db41
vgtoui <toni.uimonen@tuni.fi>
2023-11-20 17:44:50 +0200

    Make amount private

--- src/test/java/tau/springframework/MoneyTest.java
+++ src/test/java/tau/springframework/MoneyTest.java
@@ -3,13 +3,22 @@ package tau.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
+        product = five.times(3);
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
